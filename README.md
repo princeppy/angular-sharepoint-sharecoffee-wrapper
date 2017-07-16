69ebc6c56e2f265e8450ebc685671d691bab1047
Prince Park <prince_ppy@yahoo.com>
2017-07-16 22:21:01 +0400

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Angular Sharepoint ShareCoffee Extension Repository 
+
+
+## License
+
+Script is licensed under MIT license with one exception: Do not create a public WordPress plugin based on it, as I will develop it. If you need to use it for a public WordPress plugin right now, please ask me by email first. Thanks!
+
+Attribution is not required, but much appreciated, especially if you’re making a product for developers.
+
+## About
+
+Angular Sharepoint ShareCoffee Extension Repository 1.0+ is developed by [Prince Park]](http://www.princepy.com). But initially script was created by [ExpertsInside](https://github.com/ExpertsInside/angular-sharepoint).
+
+
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+// Include Gulp
+var gulp = require('gulp');
+var args = require('yargs').argv;
+var del = require('del');
+var path = require('path');
+var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*'],
+    replaceString: /\bgulp[\-.]/,
+    lazy: true
+});
+
+var config = {
+    src: 'src/',
+    dest: 'dist/',
+};
+
+/**
+ * List the available gulp tasks
+ */
+gulp.task('help', $.taskListing);
+gulp.task('default', ['help']);
+
+/**
+ * Copy js
+ * @return {Stream}
+ */
+gulp.task('js', function () {
+    return gulp.src([config.src + 'module.js', config.src + 'js/**/*.js'])
+        .pipe($.filter('**/*.js'))
+        .pipe($.order([
+            'module.js',
+            '*.app.js',
+            '*.module.js',
+            '*'
+        ]))
+        .pipe($.using({}))
+        //.pipe($.uglify())
+        .pipe($.concat('angular-sharepoint-sharecoffee-wrapper.js'))
+        .pipe(gulp.dest(config.dest))
+        .pipe($.rename('angular-sharepoint-sharecoffee-wrapper.min.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest(config.dest));
+});
+
+gulp.task('bump-patch', function(){
+    gulp.src('./*.json')
+        .pipe($.bump({type:'patch'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-minor', function(){
+    gulp.src('./*.json')
+        .pipe($.bump({type:'minor'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bump-major', function(){
+    gulp.src('./*.json')
+        .pipe($.bump({type:'major'}))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', ['js']);
